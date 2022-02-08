f59aab680f356b024008c72d0211372b68a8e167
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2022-02-08 23:14:40 +0300

    test hello world

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save user without email" do
+    u = User.new
+    assert_not u.save
+  end  
 end
